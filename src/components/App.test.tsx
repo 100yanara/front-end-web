d659670cb21e587e01d49ab9009eaa0af699d413
kimsh0117 <crea.id.1703@gmail.com>
2020-03-08 00:38:27 +0300

    add routing in App

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('full app rendering/navigating', () => {
+  const history = createMemoryHistory();
+  const { container } = render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+  /**
+   * verify page content for expected route
+   */
+  expect(container.innerHTML).toMatch('home page');
+});
+
+test('landing on a bad page shows 404 page', () => {
+  const history = createMemoryHistory();
+  history.push('/some/bad/route');
+  const { getByRole } = render(
+    <Router history={history}>
+      <App />
+    </Router>
+  );
+  expect(getByRole('heading')).toHaveTextContent('404 Not Found');
 });
