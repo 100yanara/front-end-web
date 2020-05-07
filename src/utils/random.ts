9fabcb6b585a431d10e8a93b9a49987c0007feb4
kimsh0117 <crea.id.1703@gmail.com>
2020-05-07 23:08:56 +0300

    add random color function

--- src/utils/random.ts
+++ src/utils/random.ts
@@ -1,3 +1,11 @@
 export function randomInt(min: number, max: number): number {
   return min + Math.floor((max - min) * Math.random());
 }
+export function getRandomColor(): string {
+  let letters: string = '0123456789ABCDEF';
+  let color: string = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
